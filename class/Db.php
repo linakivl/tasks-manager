88214d709f745ace955d9d570ceb9aae634ddcd7
Charalampos Sykovaridis <cs@itrust.gr>
2020-07-15 17:01:08 +0300

    again

---
+++ class/Db.php
@@ -0,0 +1,51 @@
+<?php
+
+class Db {
+    private $servername = "localhost";
+    private $username = "root";
+    private $password = "root";
+    private $database = "lina";
+
+    private $connection;
+
+    private static $instance = null;
+
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new Db();
+        }
+
+        return self::$instance;
+    }
+
+    private function __construct()
+    {
+        $this->connection = new PDO("mysql:host=$this->servername;dbname=$this->database", $this->username, $this->password);
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function execute($sql) {
+        $this->connection->exec($sql);
+    }
+
+    public function getResults($sql, $single = false) {
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute();
+
+        // set the resulting array to associative
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+        $results = $stmt->fetchAll();
+
+        if(!$results) {
+            return [];
+        }
+
+        if($single) {
+            return $results[0];
+        }
+
+        return $results;
+    }
+}
--- index.php
+++ index.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-    adasdsad
+<?php
 
-</body>
-</html>
+    require_once './class/Task.php';
+
+    $task = new Task(45454);
+
+//    $task->description = 'sdsd';
+//    $task->title = 'ttatata';
+//    $task->save();
